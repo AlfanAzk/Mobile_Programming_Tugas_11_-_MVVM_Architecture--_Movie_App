98e35409e3ba9bb3d6733e77504137229f634e1c
Muhammad Alfan Azkia <malfanzk@std.unissula.ac.id>
2023-01-22 18:20:27 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas11_mvvmarchitecturemovieapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tugas11_mvvmarchitecturemovieapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
